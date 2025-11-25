723f30d32d03750d666ece864af82af955e8cb65
Kanael-dev <kanael.dev@gmail.com>
2025-11-25 16:08:33 -0400

    Ajuste no front end pagina de login

---
+++ javascript/login.js
@@ -0,0 +1,13 @@
+function getInformationsUser() {
+    const email = document.getElementById("userEmail").value;
+    const password = document.getElementById("userPassword").value;
+
+    const DataUsers = {
+        "email": email,
+        "password": password
+    }
+
+    console.log("Email");
+    console.log("Senha");
+
+}
