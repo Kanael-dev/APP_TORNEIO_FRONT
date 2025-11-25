f893b5997ff26299b0cd047554a1e8141b24e64a
Kanael-dev <kanael.dev@gmail.com>
2025-11-25 19:29:37 -0400

    Ajuste na conexao de admin e pagina de home

--- javascript/login.js
+++ javascript/login.js
@@ -1,13 +1,43 @@
+const urlAPI = "https://app-torneio.onrender.com"
+const RotaLogin = "/login"
+
+
 function getInformationsUser() {
     const email = document.getElementById("userEmail").value;
     const password = document.getElementById("userPassword").value;
 
+    requestUser(email,password)
+}
+
+function requestUser(userEmail, userSenha){
+
+    const url = urlAPI + RotaLogin
+
     const DataUsers = {
-        "email": email,
-        "password": password
+        "email": userEmail,
+        "password": userSenha
     }
 
-    console.log("Email");
-    console.log("Senha");
+    fetch(url, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(DataUsers)
+        }
+    )
+    .then(response => response.json())
+    .then(data => {
+        console.log("Token gerado: " + data["token"])
+        localStorage.setItem("authToken", data["token"])
+        redirecionarUser()
+    })
+    .catch(err => {
+        console.error(err)
+    })
+}
 
+
+function redirecionarUser(){
+    window.location.href="./home.html"
 }
