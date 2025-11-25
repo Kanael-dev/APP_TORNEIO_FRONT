96801482e4681cdc8d18739c067b34c5e2c7ba19
nanbreno <brenoreenan@gmail.com>
2025-11-24 21:03:29 -0400

    Criação da tela de login

---
+++ css/Pages/login.js
@@ -0,0 +1,8 @@
+function getInformationsUser() {
+    const email = document.getElementById("userEmail").value ;
+    const password = document.getElementById("UserPassword").value;
+
+    console.log("Email");
+    console.log("Senha");
+
+}
