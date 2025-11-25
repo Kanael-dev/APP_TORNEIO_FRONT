28417b9a85dd5240385014de406e5fc765c9f3e8
Kanael Kenny <125425208+Kanael-dev@users.noreply.github.com>
2025-11-25 10:29:44 +0100

    Criação do objecto DataUsers

--- css/Pages/login.js
+++ css/Pages/login.js
@@ -2,6 +2,13 @@ function getInformationsUser() {
     const email = document.getElementById("userEmail").value ;
     const password = document.getElementById("UserPassword").value;
 
+    const DataUsers = {
+        "email": email,
+        "password": password
+    }
+
+    console.log("Object created: " + DataUsers)
+    
     console.log("Email");
     console.log("Senha");
 
